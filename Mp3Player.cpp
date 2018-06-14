41ac03278e5050a6f524928883fa37c8ec973604
matteo <matt.simonetti@hotmail.it>
2018-06-14 11:42:27 +0200

    Written CMakeLists and added Mp3Player class

---
+++ Mp3Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matteo on 14/06/18.
+//
+
+#include "Mp3Player.h"
---
+++ Mp3Player.h
@@ -0,0 +1,14 @@
+//
+// Created by matteo on 14/06/18.
+//
+
+#ifndef MP3PLAYER_MP3PLAYER_H
+#define MP3PLAYER_MP3PLAYER_H
+
+
+class Mp3Player {
+
+};
+
+
+#endif //MP3PLAYER_MP3PLAYER_H
