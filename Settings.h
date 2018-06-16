bc01de7678086bc8721b8725d2a2e7f211d21817
matteo <matt.simonetti@hotmail.it>
2018-06-16 14:34:02 +0200

    Added Settings class

---
+++ Settings.h
@@ -0,0 +1,17 @@
+//
+// Created by matteo on 16/06/18.
+//
+
+#ifndef MP3PLAYER_SETTINGS_H
+#define MP3PLAYER_SETTINGS_H
+
+
+class Settings {
+
+public:
+    float savedVolume;
+
+};
+
+
+#endif //MP3PLAYER_SETTINGS_H
