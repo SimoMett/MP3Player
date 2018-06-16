474b0a82c1b22ad4d1b0e2dd8e013e7f3c542bc7
matteo <matt.simonetti@hotmail.it>
2018-06-16 14:42:01 +0200

    Definition of Track class

--- Track.h
+++ Track.h
@@ -5,8 +5,28 @@
 #ifndef MP3PLAYER_TRACK_H
 #define MP3PLAYER_TRACK_H
 
+#include <string>
+
+using namespace std;
 
 class Track {
+
+public:
+    Track();
+
+    string title;
+    string artist;
+    string album;
+    string genre;
+    //bool like;
+
+    int getDuration();
+    string & getDirectory();
+
+private:
+
+    int duration;
+    string directory;
 
 };
 
