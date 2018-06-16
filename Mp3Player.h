f547ec8516ce06d23d550166fb0eb81333cb381a
matteo <matt.simonetti@hotmail.it>
2018-06-16 14:14:43 +0200

    Implementation of Mp3Player class

--- Mp3Player.h
+++ Mp3Player.h
@@ -6,14 +6,24 @@
 #define MP3PLAYER_MP3PLAYER_H
 
 #include <wx/wx.h>
+#include <vector>
+#include <memory>
 #include "Subject.h"
+#include "PlayList.h"
+#include "Track.h"
+
+using namespace std;
 
 class Mp3Player : public Subject{
 
 public:
     Mp3Player();
 
+    PlayList mainLibrary;
+    vector <shared_ptr<PlayList>> playlists;
+    float volume;
 
+    Track & currentTrack;
 
 };
 
