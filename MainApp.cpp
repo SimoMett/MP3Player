746c91f1e9cf477b134717e21aa9107a1522bebb
matteo <matt.simonetti@hotmail.it>
2018-06-18 11:45:03 +0200

    Small refactor in OnInit

--- MainApp.cpp
+++ MainApp.cpp
@@ -16,13 +16,13 @@ bool MainApp::OnInit()
 
     SetAppDisplayName("iPoorTunez");
     MainFrame * mainFrame=new MainFrame("iPoorTunes",wxPoint(50,50),wxSize(1024,728));
-    if(mainFrame)
-    {
-        wxBitmap bitmap("splash_screen.png",wxBITMAP_TYPE_PNG);
-        wxSplashScreen splash(bitmap,wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,2000,nullptr,-1);
-        wxYield();
-        wxSleep(2);
-        mainFrame->Show(true);
-    }
+
+    //if(mainFrame!= nullptr) //è sempre vera??
+    wxBitmap bitmap("splash_screen.png",wxBITMAP_TYPE_PNG);
+    wxSplashScreen splash(bitmap,wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,2000,nullptr,-1);
+    wxYield();
+    wxSleep(2);
+    mainFrame->Show(true);
+
     return mainFrame!= nullptr;
 }
