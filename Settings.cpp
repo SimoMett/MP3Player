d8090464e32d5adc47a1c9e04c7d612245563450
matteo <matt.simonetti@hotmail.it>
2018-06-21 17:09:56 +0200

    Implementing Settings class

--- Settings.cpp
+++ Settings.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Settings.h"
+
+Settings::Settings(string settingsfile)
+{
+    if(!wxFileExists(settingsfile))
+    {
+        wxFile settings(settingsfile,wxFile::OpenMode::write_excl);
+        settings.Close();
+    }
+
+    LoadSettings(settingsfile);
+}
+
+Settings::~Settings()
+{
+    SaveSettings();
+}
+
+void Settings::LoadSettings(string file)
+{
+
+}
+
+void Settings::SaveSettings()
+{
+
+}
--- Settings.h
+++ Settings.h
@@ -5,12 +5,24 @@
 #ifndef MP3PLAYER_SETTINGS_H
 #define MP3PLAYER_SETTINGS_H
 
+#include <fstream>
+#include <wx/wx.h>
+#include <wx/xml/xml.h>
+#include <wx/file.h>
+#include <string>
+
+using namespace std;
 
 class Settings {
 
 public:
-    float savedVolume;
+    explicit Settings(string settingsfile="settings.xml");
+    ~Settings();
 
+    float savedVolume;
+private:
+    void LoadSettings(string file);
+    void SaveSettings();
 };
 
 
