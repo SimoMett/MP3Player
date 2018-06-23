e27978e2b0f1e27e5c680963cb6a7ef92f58c0a4
matteo <matt.simonetti@hotmail.it>
2018-06-23 19:24:53 +0200

    Slider now inherits from wxSlider class

--- Slider.cpp
+++ Slider.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Slider.h"
+
+Slider::Slider(Mp3Player * _subject, wxWindow *parent, wxWindowID id, int value, int minValue, int maxValue, const wxPoint &pos,
+        const wxSize &size, long style, const wxValidator &validator, const wxString &name) :
+        playerInstance(_subject), wxSlider(parent,id,value,minValue,maxValue,pos,size,style,validator,name)
+
+{
+    if(playerInstance!=nullptr)
+        playerInstance->attachObserver(shared_ptr<Observer>(this));
+}
+
+
+
+Slider::~Slider()
+{
+    playerInstance->detachObserver(shared_ptr<Observer>(this));
+}
--- Slider.h
+++ Slider.h
@@ -5,20 +5,22 @@
 #ifndef MP3PLAYER_SLIDER_H
 #define MP3PLAYER_SLIDER_H
 
+
+#include <wx/wx.h>
 #include "Observer.h"
 #include "Subject.h"
+#include "Mp3Player.h"
 
-class Slider : public Observer
+class Slider : public Observer , public wxSlider
 {
 public:
-    Slider(Subject * _subject);
+    Slider(Mp3Player *_subject, wxWindow *parent, wxWindowID id, int value, int minValue, int maxValue,
+           const wxPoint &pos = wxDefaultPosition, const wxSize &size = wxDefaultSize, long style = wxSL_HORIZONTAL,
+           const wxValidator &validator = wxDefaultValidator, const wxString &name = wxSliderNameStr);
 
-    //Mp3Player * playerInstance;
-    Subject * subject;
-    int sliderValue;
+    ~Slider();
 
-    virtual void update();
-
+    Mp3Player *playerInstance;
 };
 
 
