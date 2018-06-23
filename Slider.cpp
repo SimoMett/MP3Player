8e3eebd91620f6b86a434dfadb67411e70b5cea4
matteo <matt.simonetti@hotmail.it>
2018-06-23 15:03:05 +0200

    Added Slider class

---
+++ Slider.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matteo on 23/06/18.
+//
+
+#include "Slider.h"
---
+++ Slider.h
@@ -0,0 +1,14 @@
+//
+// Created by matteo on 23/06/18.
+//
+
+#ifndef MP3PLAYER_SLIDER_H
+#define MP3PLAYER_SLIDER_H
+
+
+class Slider {
+
+};
+
+
+#endif //MP3PLAYER_SLIDER_H
