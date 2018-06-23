f4d3b34da25da929cd306a1431d4ff8b1e113906
matteo <matt.simonetti@hotmail.it>
2018-06-23 20:43:26 +0200

    Implemented TrackSlider::update()

--- TrackSlider.cpp
+++ TrackSlider.cpp
@@ -10,9 +10,8 @@ TrackSlider::TrackSlider(Mp3Player *_subject, wxWindow *parent, wxWindowID id, i
 
 void TrackSlider::update()
 {
-    float val=playerInstance->getCurrentTrackTiming();
-
-    //TODO remap of val
+    int currentTrackDuration=playerInstance->currentTrack->getDuration();
+    float val=(playerInstance->getCurrentTrackTiming()/currentTrackDuration)*wxSlider::GetMax();
 
     wxSlider::SetValue(val);
 }
