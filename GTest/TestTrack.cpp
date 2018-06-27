8e0b1cee42ae8e75812238d302c866b2e5d4cf15
matteo <matt.simonetti@hotmail.it>
2018-06-27 21:07:36 +0200

    Testing Track constructor

--- GTest/TestTrack.cpp
+++ GTest/TestTrack.cpp
@@ -3,11 +3,15 @@
 //
 
 #include <gtest/gtest.h>
+#include "../Mp3Player.h"
 #include "../Track.h"
 
 TEST(Track,Constructor)
 {
+    Mp3Player::Create();
     Track t("directory/nome.mp3");
 
     ASSERT_EQ("nome.mp3",t.title);
+
+
 }
