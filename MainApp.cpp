93eabe505afa1b04d103ceb1f6f84132061edf1f
matteo <matt.simonetti@hotmail.it>
2018-07-01 00:37:15 +0200

    Hello UTunes!!

--- MainApp.cpp
+++ MainApp.cpp
@@ -17,8 +17,6 @@ bool MainApp::OnInit()
     if(argc > 1)
         cout << argv[1]<<endl;
 
-
-
     //SetAppDisplayName("iPoorTunez");
     MainFrame * mainFrame=new MainFrame("UTunes",wxPoint(50,50),wxSize(1024,728));
 
