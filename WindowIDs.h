3a98fe0fc92636ccaeeebcfaf9efa726f18c6f8a
matteo <matt.simonetti@hotmail.it>
2018-07-02 18:14:04 +0200

    Added WindowIDs.h

---
+++ WindowIDs.h
@@ -0,0 +1,24 @@
+//
+// Created by matteo on 02/07/18.
+//
+
+#ifndef MP3PLAYER_WINDOWIDS_H
+#define MP3PLAYER_WINDOWIDS_H
+
+#include <wx/wx.h>
+
+enum
+{
+    ID_Open = wxID_HIGHEST+1,
+    ID_TrackSlider=wxID_HIGHEST+2,
+    ID_PlayButton=wxID_HIGHEST+3,
+    ID_Credits=wxID_HIGHEST+4,
+    ID_NextTrackButton=wxID_HIGHEST+5,
+    ID_PrevTrackButton=wxID_HIGHEST+6,
+    ID_VolumeSlider=wxID_HIGHEST+7,
+    ID_MediaCtrl=wxID_HIGHEST+8,
+    ID_NewPlayLst=wxID_HIGHEST+9,
+    ID_PlayLstBox=wxID_HIGHEST+10
+};
+
+#endif //MP3PLAYER_WINDOWIDS_H
