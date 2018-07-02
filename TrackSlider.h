1be30c8cdde7b078a6e9a6a1d5b4e7a0fe9ee216
matteo <matt.simonetti@hotmail.it>
2018-07-02 18:16:27 +0200

    Slider now accept Subjects, no more Mp3Player. Added a wxTimer to retrieve current track timing

--- TrackSlider.h
+++ TrackSlider.h
@@ -5,19 +5,23 @@
 #ifndef MP3PLAYER_TRACKSLIDER_H
 #define MP3PLAYER_TRACKSLIDER_H
 
+#include <wx/timer.h>
 #include "Slider.h"
 
 class TrackSlider : public Slider {
 public:
-    TrackSlider(Mp3Player *_subject, wxWindow *parent, wxWindowID id, int value, int minValue, int maxValue,
-                 const wxPoint &pos = wxDefaultPosition, const wxSize &size = wxDefaultSize, long style = wxSL_HORIZONTAL,
-                 const wxValidator &validator = wxDefaultValidator, const wxString &name = wxSliderNameStr);
+    TrackSlider(Subject *_subject, wxWindow *parent, wxWindowID id);
+    ~TrackSlider();
 
     void update(Subject * subject) final ;
 
     virtual void OnValueChanged() override ;
-};
 
+private:
+
+    wxTimer timer;
+    void OnTimer(wxTimerEvent & event);
+};
 
 
 #endif //MP3PLAYER_TRACKSLIDER_H
