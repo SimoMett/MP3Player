4ef96f42ff6bf97df470259f0f7a9130476779fb
matteo <matt.simonetti@hotmail.it>
2018-07-05 15:06:16 +0200

    Replaced raw pointer with smart pointer

--- GTest/TestMp3Player.cpp
+++ GTest/TestMp3Player.cpp
@@ -2,13 +2,14 @@
 // Created by matteo on 24/06/18.
 //
 
+#include <memory>
 #include <gtest/gtest.h>
 #include "../Mp3Player.h"
 
 TEST(Mp3Player,Contructor)
 {
     Mp3Player::Create();
-    Mp3Player * p= Mp3Player::getInstancePtr();
+    unique_ptr<Mp3Player> & p = Mp3Player::getInstancePtr();
     ASSERT_EQ("#mainLibrary",p->mainLibrary.getName());
 }
 
